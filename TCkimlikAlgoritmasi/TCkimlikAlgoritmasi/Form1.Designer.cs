57db7555b671d1c1ea84ee5f7a8da09e5e9e4acb
yaysan00 <yaysan00@users.noreply.github.com>
2020-08-12 17:32:17 +0300

    TC Kimlik Algoritması
    Girilen TC Kimlik numarasının geçerli olup olmadığını doğrulayan ve TC Kimlik numarası üreten algoritma.

---
+++ TCkimlikAlgoritmasi/TCkimlikAlgoritmasi/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TCkimlikAlgoritmasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textTC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Doğrula";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textTC
+            // 
+            this.textTC.Location = new System.Drawing.Point(12, 33);
+            this.textTC.Name = "textTC";
+            this.textTC.Size = new System.Drawing.Size(139, 22);
+            this.textTC.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TC Kimlik Doğrulama";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Oluştur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(15, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(136, 84);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "TC Kimlik Oluşturucu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 231);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textTC);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textTC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        internal System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TCkimlikAlgoritmasi/TCkimlikAlgoritmasi/TcKimlik.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCkimlikAlgoritmasi
+{
+    class TCKimlik
+    {
+                
+        int onuncuSayi = 0;int SonSayi = 0;
+        string tc = "";
+        public void TekCift(string TCNumarasi)
+        {
+            
+            int tekToplam = 0; int ciftToplam = 0;
+            for (int i = 0; i < 9; i += 2)
+            { // 1,3,5,7,9 haneleri toplar
+                tekToplam += Convert.ToInt32(TCNumarasi[i].ToString());
+            }
+
+            for (int i = 1; i < 9; i += 2)
+            { // 2,4,6,8 haneleri toplar
+                ciftToplam += Convert.ToInt32(TCNumarasi[i].ToString());
+            }
+
+            onuncuSayi = ((tekToplam * 7) - ciftToplam) % 10; // 10.Sırada ki Sayıyı buldum
+            SonSayi = (tekToplam + ciftToplam + onuncuSayi) % 10;// 11.Sırada ki sayıyı buldum
+        }
+
+
+        public string Dogrula(string TCNumarasi)
+        {
+            if (TCNumarasi.Length == 11 && Convert.ToInt32(TCNumarasi[0].ToString()) != 0)
+            {
+                if (Convert.ToInt32(TCNumarasi[9].ToString()) == onuncuSayi && Convert.ToInt32(TCNumarasi[10].ToString()) == SonSayi)
+                // 10. ve 11. sırada ki sayıları karşılaştırır.
+                {
+                    MessageBox.Show("Geçerli TC Kimlik Numarası");
+                    //return true;    
+                }
+                else
+                {
+                    MessageBox.Show("Geçersiz TC Kimlik Numarası");
+                    //return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show(" TC Kimlik Numarası Sıfırdan Farklı Bir Sayıyla Başlamalıdır. " +
+                    "\n TC Kimlik 11 Haneden oluşmalıdır.");
+                //return false;
+            }
+            return TCNumarasi;
+
+        }
+        public string Olustur()
+        {
+            
+            Random random = new Random();
+            for (int i = 0; i < 9; i++)
+            {
+                tc += Convert.ToString(random.Next(1, 9));
+            }
+             TekCift(tc);
+             string olusanTC = tc + onuncuSayi.ToString() + SonSayi.ToString();
+             return Dogrula(olusanTC);
+
+        }     
+    }
+}
